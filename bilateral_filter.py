9d41637db5a6e86738030025f190c686a23231e5
Aurélien PIERRE <aurelien@cellierdomesticus.ca>
2017-04-30 02:40:10 -0400

    Optimize, simplify, and re-write in a more pythonic way
    Divide execution time by a lot
    Take advantage of Numpy arrays
    Clean functions output
    Replace while loops by for loops
    Take care of image padding
    Etc.

--- bilateral_filter.py
+++ bilateral_filter.py
@@ -1,66 +1,49 @@
 import numpy as np
-import cv2
+from PIL import Image
 import sys
-import math
-
-
-def distance(x, y, i, j):
-    return np.sqrt((x-i)**2 + (y-j)**2)
+import scipy.signal
 
 
 def gaussian(x, sigma):
-    return (1.0 / (2 * math.pi * (sigma ** 2))) * math.exp(- (x ** 2) / (2 * sigma ** 2))
+    return (1.0 / (2 * np.pi * (sigma ** 2))) * np.exp(- (x ** 2) / (2 * sigma ** 2))
 
 
-def apply_bilateral_filter(source, filtered_image, x, y, diameter, sigma_i, sigma_s):
-    hl = diameter/2
-    i_filtered = 0
-    Wp = 0
-    i = 0
-    while i < diameter:
-        j = 0
-        while j < diameter:
-            neighbour_x = x - (hl - i)
-            neighbour_y = y - (hl - j)
-            if neighbour_x >= len(source):
-                neighbour_x -= len(source)
-            if neighbour_y >= len(source[0]):
-                neighbour_y -= len(source[0])
-            gi = gaussian(source[neighbour_x][neighbour_y] - source[x][y], sigma_i)
-            gs = gaussian(distance(neighbour_x, neighbour_y, x, y), sigma_s)
+def bilateral_filter(source, radius, std_i, std_s):
+    filtered_image = np.zeros_like(source).astype(float)
+    W = 0
+
+    pad = np.pad(source, (radius, radius), mode="symmetric")
+
+    for i in range(-radius, radius):
+        for j in range(-radius, radius):
+
+            neighbour = pad[radius + i: radius + i + source.shape[0],
+                            radius + j: radius + j + source.shape[1]]
+
+            distance_x = (i)**2
+            distance_y = (j)**2
+            distance = np.sqrt(distance_x + distance_y)
+
+            gi = gaussian((neighbour - source), std_i)
+            gs = gaussian(distance, std_s)
+
             w = gi * gs
-            i_filtered += source[neighbour_x][neighbour_y] * w
-            Wp += w
-            j += 1
-        i += 1
-    i_filtered = i_filtered / Wp
-    filtered_image[x][y] = int(round(i_filtered))
+            W += w
+            filtered_image += neighbour * w
 
-
-def bilateral_filter_own(source, filter_diameter, sigma_i, sigma_s):
-    filtered_image = np.zeros(source.shape)
-
-    i = 0
-    while i < len(source):
-        j = 0
-        while j < len(source[0]):
-            apply_bilateral_filter(source, filtered_image, i, j, filter_diameter, sigma_i, sigma_s)
-            j += 1
-        i += 1
-    return filtered_image
+    return np.around(filtered_image / W).astype(np.uint8)
 
 
 if __name__ == "__main__":
+
     src = cv2.imread(str(sys.argv[1]), 0)
-    filtered_image_OpenCV = cv2.bilateralFilter(src, 5, 12.0, 16.0)
-    cv2.imwrite("original_image_grayscale.png", src)
-    cv2.imwrite("filtered_image_OpenCV.png", filtered_image_OpenCV)
-    filtered_image_own = bilateral_filter_own(src, 5, 12.0, 16.0)
-    cv2.imwrite("filtered_image_own.png", filtered_image_own)
+    dest = "filtered_image_own.png"
 
+    with Image.open(src) as pic:
 
+        pic = np.array(pic)
 
+        filtered_image = bilateral_filter(pic, 5, 1, 3)
 
-
-
-
+        with Image.fromarray(filtered_image) as output:
+            output.save(dest, mode="L")
